6137d6a6397d838c381f01253b4407826c5a4d9b
Indra Tanaya <aryaindratanaya@gmail.com>
2022-05-03 18:20:48 +0800

    Implement event binding when a hero is clicked

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -7,6 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+
+    console.log(this.selectedHero);
+  }
+
   heroes = HEROES;
 
   constructor() {}
