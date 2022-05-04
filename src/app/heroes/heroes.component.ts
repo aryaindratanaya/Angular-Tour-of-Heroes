7ef304872ffbcee82c6fe9cff4fbc79c07af5e37
Indra Tanaya <aryaindratanaya@gmail.com>
2022-05-04 22:59:34 +0800

    Remove console logs

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,6 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-
-    console.log(this.selectedHero);
   }
 
   heroes = HEROES;
